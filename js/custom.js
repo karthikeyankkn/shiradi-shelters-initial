46c3ae7cab081b939d706436932320436760282f
Baskarbass7 <baskarbass@yahoo.com>
2022-05-11 13:58:09 +0530

    banner removed

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
 // find url segment
 	var url = location.href
-	// var segment = url.split("/").length - 2 - (url.indexOf("http://")==-1?0:2);
-	var segment = 0;
+	var segment = url.split("/").length - 2 - (url.indexOf("http://")==-1?0:2);
 	console.log("Segment: "+segment);
 
 		if (segment == 0) {
-			// $("nav").load("header.html"); 
+			$("nav").load("header.html"); 
 	  	$("footer").load("footer.html"); 
 		}else{
 			console.log("Js Header will not work for this page");
